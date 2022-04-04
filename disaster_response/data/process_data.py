4f14cebd65b29274907d199ade1526e648e8def1
pcmaldonado <pcmaldonado.o@gmail.com>
2022-04-04 17:07:00 +0200

    uploaded scripts for data cleaning & modeling

---
+++ disaster_response/data/process_data.py
@@ -0,0 +1,71 @@
+# ===== LIBRARIES ======
+# Basic libraries to handle data
+import numpy as np
+import pandas as pd
+
+# To load data to SQL database
+from sqlalchemy import create_engine
+
+# To access configuration variables
+import sys; sys.path.append('..')
+from pathlib import Path
+from disaster_response.config.core import DATASET_DIR, config 
+
+
+# ===== FUNCTIONS ======
+def load_data(messages_file_name:str, categories_file_name:str):
+    """Loads and merge the messages & categories datasets"""
+    # Loads datasets
+    messages = pd.read_csv(Path(f'{DATASET_DIR/messages_file_name}'))
+    categories = pd.read_csv(Path(f'{DATASET_DIR/categories_file_name}'))
+    
+    # Merges the two datasets
+    data = messages.merge(categories, on='id')
+    
+    return data
+
+
+def clean_data(data):
+    """Loads data, split categories into multiple columns, 
+    then remove duplicates"""
+    # ===== Transforming "Categories" ======
+    # Splitting categories (one col) into n columns
+    cats = data.categories.str.split(';', expand=True)
+    cols = cats.iloc[0,:].apply(lambda x:x[:-2])
+    cats.columns = cols  
+    for column in cats:
+        cats[column] = pd.to_numeric(cats[column].str[-1])
+        
+    # Concat categories with the rest of the data
+    data = pd.concat([data.drop(['categories'], axis=1), cats], axis=1)
+    
+    # ===== Removing duplicates ======
+    # Removing unnecesary feature
+    data = data.drop_duplicates()
+    
+    return data
+
+
+def save_data(df):
+    """Saves cleaned data into an sqlite database"""
+    save_file_name = config.app_config.database_file_name
+    table_name = config.app_config.table_name
+    
+    engine = create_engine(f'sqlite:///{DATASET_DIR}/{save_file_name}')
+    df.to_sql(f'{table_name}', engine, index=False, if_exists='replace')
+    
+    
+def etl_process():
+    """Contains the 3 steps of the ETL pipeline"""
+    df = load_data(messages_file_name = config.app_config.messages_file_name,
+                   categories_file_name = config.app_config.categories_file_name)  
+    print('extracted data')
+    df = clean_data(df)
+    print('transformed data')
+    save_data(df)
+    print('loaded data')
+    
+    
+if __name__ == '__main__':
+    # run data pipeline
+    etl_process()
---
+++ disaster_response/models/train_classifier.py
@@ -0,0 +1,163 @@
+# ===== LIBRARIES ======
+# To handle data
+import pandas as pd
+from sqlalchemy import create_engine
+
+# To access configuration 
+import sys; sys.path.append('..')
+from disaster_response.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config
+
+# To preprocess text data
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('omw-1.4')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+
+import re
+
+# To build ML pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import make_scorer, f1_score, recall_score, precision_score
+
+# To get execution time 
+import time
+
+# To save model
+import joblib
+
+
+# ===== FUNCTIONS ======
+def prepare_data():
+    """It loads the data from the SQL database,
+    separate targets and features, then splits train/test sets 
+    and returns X_train, X_test, y_train, y_test"""
+    # load data from database
+    database_file_name = config.app_config.database_file_name
+    table_name = config.app_config.table_name
+    engine = create_engine(f'sqlite:///{DATASET_DIR}\\{database_file_name}')
+    df = pd.read_sql(f'SELECT * FROM {table_name}', engine)
+
+    # Separate features from targets
+    X = df[config.model_config.features]
+    y = df[config.model_config.targets]
+    
+    # Simplification to avoid multiclass problems (plus, class 2 is highly imbalanced)
+    y.related = y.related.replace({2:1})
+    
+    # Splits data into train/test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    print('Data ready for modeling')
+    return X_train, X_test, y_train, y_test
+
+    
+def nlp_pipeline(text):
+    '''Normalize and tokenize input text,
+    then applies stemming and lemmatization,
+    finally returns cleaned text'''
+    
+    # Makes all text lowercase then keeps only alphabetical characters
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z]',' ',text)
+    
+    # Tokenize text 
+    text = word_tokenize(text)
+    
+    # Stopwords
+    omit = ['no', 'but']
+    text = [t for t in text if t not in set(stopwords.words('english')) - set(omit)]
+    
+    # Stemming text
+    text = [PorterStemmer().stem(t) for t in text]
+
+    # Lemmatize text
+    text = [WordNetLemmatizer().lemmatize(t) for t in text]
+    
+    return text
+
+
+def build_model():
+    # ML pipeline
+    """It builds the pipeline and sets the GridSearchCV parameters"""
+    model_pipeline = Pipeline([('vect', CountVectorizer(tokenizer=nlp_pipeline)), 
+                     ('tfidf', TfidfTransformer()), 
+                     ('clf', RandomForestClassifier(n_estimators=50, random_state=42, n_jobs=-1))
+                    ])
+     
+    param_grid = {
+        'vect__max_df': (0.7, 0.8), #Ignore terms that have a frequency higher than the given threshold
+        'tfidf__use_idf': (True, False), #Enable inverse-document-frequency reweighting
+        'clf__max_features': ['sqrt', 'log2'], #Number of features to consider when looking for the best split
+    }
+    
+    scorer = make_scorer(f1_score, average='weighted', zero_division=0)
+    model_pipeline = GridSearchCV(model_pipeline, param_grid=param_grid, scoring=scorer, cv=3, verbose=2)
+        
+    return model_pipeline
+
+
+def train_model(X_train, y_train):
+    """Trains the model on the train set"""
+   # Training Model
+    model = build_model()
+
+    print('Training the ML model')
+    startTime = time.time()
+    model.fit(X_train, y_train)
+
+    # Getting best hyperparameteres from GridSearchCV
+    print('Done training')
+
+    executionTime = (time.time() - startTime)
+    print('Execution time in minutes: ' + str(executionTime/60))
+    print('Best hyperpameters:', model.best_params_)
+    
+    return model
+    
+
+def evaluate_model(model, X_test, y_test):
+    """Predicts on test data then shows performance metrics (weighted f1-score)"""
+    # Predicts on test data
+    y_pred = model.predict(X_test)
+    target_names = y_test.columns
+    
+    # Prints performance on test data by category
+    print('\n\nPerformance by category:')
+    print('-------------------------')
+    for i in range(len(target_names)):
+        f1score = f1_score(y_test.iloc[:, i].values, y_pred[:, i], average='weighted', zero_division=0)
+        recall = recall_score(y_test.iloc[:, i].values, y_pred[:, i], average='weighted', zero_division=0)
+        precision = precision_score(y_test.iloc[:, i].values, y_pred[:, i], average='weighted', zero_division=0)
+        print(f'Category: {target_names[i]}')
+        print(f'F1-score: {f1score}')
+        print(f'Recall: {recall}')
+        print(f'Precision: {precision}\n\n')
+
+
+def export_model(model):
+    """Saves model as pickle file"""
+    # Saving model
+    save_file_name = f'{config.app_config.model_save_file}'
+    save_path = TRAINED_MODEL_DIR / save_file_name
+    joblib.dump(value=model, filename=save_path, compress=3)
+    print('Model saved')
+
+
+def run_pipeline():
+    """Runs the entire ML pipeline"""
+    X_train, X_test, y_train, y_test = prepare_data()
+    model = train_model(X_train, y_train)
+    evaluate_model(model, X_test, y_test)
+    export_model(model)
+    
+
+if __name__ == '__main__':
+    # run ML pipeline
+    run_pipeline()
