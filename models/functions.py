de323023377c4121e3e56d45569ccb511e492a7d
pcmaldonado <pcmaldonado.o@gmail.com>
2022-04-05 18:22:38 +0200

    retesting running from database

--- models/functions.py
+++ models/functions.py
@@ -55,11 +55,12 @@ def tokenize(text):
 
 
 def load_clean_data():
-    # database_file_name = config.app_config.database_file_name
-    # table_name = config.app_config.table_name
-    # engine = create_engine(f'sqlite:///{DATASET_DIR}\\{database_file_name}')
-    # df = pd.read_sql_table(f'{table_name}', engine)
-    df = pd.read_csv(DATASET_DIR / 'DisasterResponse.csv')
+    database_file_name = config.app_config.database_file_name
+    table_name = config.app_config.table_name
+    engine = create_engine(f'sqlite:///{DATASET_DIR}\\{database_file_name}')
+    df = pd.read_sql_table(f'{table_name}', engine)
+    # df = pd.read_csv(DATASET_DIR / 'DisasterResponse.csv')
+    
     # Separate features from targets
     features = df[config.model_config.features]
     targets = df[config.model_config.targets]
